2a53c62db736b1b4e509ddbc09437566e4497f3e
zhangyong <nickzylove@gmail.com>
2016-05-09 14:42:46 +0800

    this is first commit on a branch other than the main branch

--- examplecsv.py
+++ examplecsv.py
@@ -1,15 +1,18 @@
-## this code cannot be run cause I am too lazy to produce the csv file
+# !/usr/local/bin/python
+# -*- coding: utf-8 -*-
 
 import csv
 
-with open('extension.csv') as csvfile:
+with open('datecolor.csv') as csvfile:
+    # read a csv file, the row of the fiel is separated by ','
     readCSV = csv.reader(csvfile,delimiter=',')
 
     dates=[]
     colors=[]
     
+    # get into the csv.reader object
     for row in readCSV:
-        color=row[2]
+        color=row[1]
         date=row[0]
 
         dates.append(date)
@@ -19,16 +22,16 @@ with open('extension.csv') as csvfile:
     print(colors)
     
     try:
-        whatcolor=input('what color do you wish to know the date of?')
+        # user can input using `raw_input` in python
+        whatcolor=raw_input('what color do you wish to know the date of?')
 
         if whatcolor in colors:
-            coldex = colors.index(whatcolor.upper())
+            # get the index of an element in a list. If multiple the same, the first one's index is extracted
+            coldex = colors.index(whatcolor)
             theDate = dates[coldex]
             print('The date of', whatcolor, 'is', theDate)
         else:
             print('Not found')
     # except Exception, e ## this what happens in python2
-    except Exception as e:
+    except Exception, e:
         print(e)
-
-    print('continuing')
